b07ac25a74d59582e978d82f05ede5c7129c5e27
Ciro <67671434+cirolpz@users.noreply.github.com>
2025-03-28 21:12:07 -0300

    Delete src/app/proyectos directory

--- src/app/proyectos/proyectos.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-proyectos',
-  templateUrl: './proyectos.component.html',
-  styleUrls: ['./proyectos.component.css'],
-  standalone: true
-})
-export class ProyectosComponent { }
